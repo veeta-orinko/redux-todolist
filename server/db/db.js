bc13635c19adb9e120b6894b9e194a030756a92d
Grant Scott-Hayes <107601640+grant-scott-hayes@users.noreply.github.com>
2022-09-08 20:45:22 +1200

    added some database functions

--- server/db/db.js
+++ server/db/db.js
@@ -1,14 +1,33 @@
 const connection = require('./connection')
 
-function getFruits(db = connection) {
-  return db('fruit').select()
+function getToDo(db = connection) {
+  return db('todo').select()
 }
 
 //add todo
+function addToDo(item, db = connection) {
+  return db('todo').insert({ item: item, completed: false })
+}
+
 //delete todo
+function delToDo(id, db = connection) {
+  return db('todo').delete().where('id', id)
+}
+
 //update todo
+function updateToDo(id, text, db = connection) {
+  return db('todo').where('id', id).update({ item: text })
+}
+
 //update completion status?
+function updateStatus(id, status, db = connection) {
+  return db('todo').where('id', id).update({ completed: status })
+}
 
 module.exports = {
-  getFruits,
+  updateStatus,
+  delToDo,
+  updateToDo,
+  addToDo,
+  getToDo,
 }
